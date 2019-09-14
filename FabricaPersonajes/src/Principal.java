ab3497edaedd09e070a363cdf7395d0d337f86fd
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2019-09-13 20:22:37 -0500

    Add files via upload

---
+++ FabricaPersonajes/src/Principal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import fabricas.crearCaballero;
+import fabricas.crearHechicero;
+import fabricas.crearOgro;
+import fabricas.crearPersonajeAbstracto;
+import personajes.Arma;
+import personajes.Escudo;
+
+public class Principal {
+	
+	
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		Frame f = new Frame(); 
+		
+		
+		//Scanner sc = new Scanner(System.in);
+		
+		
+		String opcion=f.getOpcion();
+		//System.out.println("opcion del cmb : "+opcion+ ".");
+					
+		
+		
+	
+		
+	
+
+}}
